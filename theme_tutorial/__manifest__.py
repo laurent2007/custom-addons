e849237ab8f9a5312cc37904773339e438b09c59
laurent2007 <laurent2007@163.com>
2019-03-15 15:20:01 +0800

    添加date_range&account_tax_balance&theme_tutorial

---
+++ theme_tutorial/__manifest__.py
@@ -0,0 +1,12 @@
+{
+  'name':'Tutorial theme',
+  'description': 'A description for your theme.',
+  'version':'1.0',
+  'author':'Laurent',
+  'data': [
+    'views/layout.xml',
+    #'views/pages.xml'
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website','website_theme_install'],
+}
